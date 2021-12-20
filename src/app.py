5fefcb24a7f6cd8e813ddf692954e2507b8844d1
Kenny <kazimirzak15@gmail.com>
2021-12-20 11:50:03 +0100

    auth removed

--- src/app.py
+++ src/app.py
@@ -1,13 +1,10 @@
-from http.client import responses
-from os import stat
-from fastapi import FastAPI, Query, HTTPException, APIRouter, Request, Header
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Query, HTTPException, APIRouter, Request
+from fastapi.params import Depends
 from typing import Optional
 import google_storage
 import mysql_storage
 import info
 import models
-import json
 
 UPLOAD_URL = '/api/minizinc/upload'
 
@@ -18,87 +15,6 @@ app = FastAPI(
     openapi_tags=info.TAGS_METADATA
 )
 router = APIRouter()
-
-def is_upload_endpoint(endpoint):
-    return endpoint == UPLOAD_URL or endpoint == f'{UPLOAD_URL}/'
-
-
-def is_delete_user(endpoint):
-    arr = endpoint.split('/')
-    if len(arr) == 5:
-        return arr[-1] == ''
-    return len(arr) == 4
-
-
-async def extract_user_id(request):
-    if (request.method == 'POST'):
-        # User id is in body
-        print("BODY")
-        body = await request.json()
-        request_userID = body['userID']
-    elif('userID' in request.query_params):
-        # User id is in query param (they are not gauranteed to be here)
-        print("QUERY PARAMS")
-        request_userID = request.query_params['userID']
-    else:
-        # Hack to obtain the userID when its in the path, might break the middleware if a new endpoint is created.
-        print("PATH PARAMS")
-        request_userID = request.url.path.split('/')[3]
-    return request_userID
-
-
-@app.middleware('http')
-async def authorize(request: Request, call_next):
-    print("Method:", request.method)
-    print("Path:", request.url.path)
-    print("Port:", request.url.port)
-    print("Scheme:", request.url.scheme)
-    print("Header UserID:", request.headers.get('userid'))
-    print("Header Role:", request.headers.get('role'))
-    print("Query Params:", request.query_params)
-    print("Path params:", request.path_params)
-    print("Client IP:", request.client.host)
-
-    if request.url.path == '/openapi.json' or request.url.path == '/docs':
-        response = await call_next(request)
-        return response
-    # edge case for requesting an upload url.
-    if is_upload_endpoint(request.url.path) and 'userID' not in request.query_params and request.method == 'GET':
-        response = await call_next(request)
-        return response
-
-    header_userID = request.headers.get('userid')
-    header_role = request.headers.get('role')
-    if not header_userID or not header_role:
-        response = JSONResponse(status_code=401)
-
-    header_userID = header_userID.strip()
-    header_role = header_role.strip()
-    
-    
-    request_userID = await extract_user_id(request)
-    request_userID = request_userID.strip()
-    if request_userID != header_userID and header_role != 'admin':
-        return JSONResponse(status_code=401)
-    
-    # Alot of checking to see if it is an admin requesting for a user to be deleted.
-    is_request_for_different_user = request_userID != header_userID
-    is_admin = header_role == 'admin'
-    is_delete = request.method == 'DELETE'
-    is_del_user = is_delete_user(request.url.path)
-    if is_request_for_different_user and is_admin:
-        if not is_delete:
-            return JSONResponse(status_code=401)
-        if not is_del_user:
-            return JSONResponse(status_code=401)
-
-    print("reuqest")
-    response = await call_next(request)
-    print("done!")
-    return response
-
-
-
 
 # Had to include double route with and without trailing backslash to please the gateway-gods
 # See: https://github.com/tiangolo/fastapi/issues/2060
